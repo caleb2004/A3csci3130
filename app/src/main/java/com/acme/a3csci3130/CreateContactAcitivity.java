35358ebfbf280b308fa91ea46d1ae142f09c252c
caleb2004 <Caleb@HANNINGdeMacBook-Pro.local>
2017-07-12 11:02:42 -0300

    add espresso test

--- app/src/main/java/com/acme/a3csci3130/CreateContactAcitivity.java
+++ app/src/main/java/com/acme/a3csci3130/CreateContactAcitivity.java
@@ -3,13 +3,15 @@ package com.acme.a3csci3130;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
 
 public class CreateContactAcitivity extends Activity {
 
-    private Button submitButton;
-    private EditText nameField, emailField;
+    private EditText nameField, addressField, busNumField;
+    private Spinner provSpin, busSpin;
     private MyApplicationData appState;
 
     @Override
@@ -19,21 +21,29 @@ public class CreateContactAcitivity extends Activity {
         //Get the app wide shared variables
         appState = ((MyApplicationData) getApplicationContext());
 
-        submitButton = (Button) findViewById(R.id.submitButton);
         nameField = (EditText) findViewById(R.id.name);
-        emailField = (EditText) findViewById(R.id.email);
+        busNumField = (EditText) findViewById(R.id.businessNumber);
+        addressField = (EditText) findViewById(R.id.address);
+        provSpin = (Spinner) findViewById(R.id.province);
+        busSpin = (Spinner) findViewById(R.id.business);
+
     }
 
     public void submitInfoButton(View v) {
         //each entry needs a unique ID
         String personID = appState.firebaseReference.push().getKey();
         String name = nameField.getText().toString();
-        String email = emailField.getText().toString();
-        Contact person = new Contact(personID, name, email);
+        String busNum = busNumField.getText().toString();
+        String address = addressField.getText().toString();
+        String province = provSpin.getSelectedItem().toString();
+        String business = busSpin.getSelectedItem().toString();
+
+        Contact person = new Contact(personID, busNum, name, business, address, province);
 
         appState.firebaseReference.child(personID).setValue(person);
 
         finish();
 
     }
+
 }
--- app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
+++ app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
@@ -1,37 +1,67 @@
 package com.acme.a3csci3130;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class DetailViewActivity extends Activity {
 
-    private EditText nameField, emailField;
-    Contact receivedPersonInfo;
+    private EditText nameField, addressField, busNumField;
+    private Spinner provinceSpin, businessSpin;
+    private Contact receivedPersonInfo;
+    private MyApplicationData appState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_view);
         receivedPersonInfo = (Contact)getIntent().getSerializableExtra("Contact");
+        appState = ((MyApplicationData) getApplicationContext());
 
         nameField = (EditText) findViewById(R.id.name);
-        emailField = (EditText) findViewById(R.id.email);
+        busNumField = (EditText) findViewById(R.id.businessNumber);
+        addressField = (EditText) findViewById(R.id.address);
+        provinceSpin = (Spinner) findViewById(R.id.province);
+        businessSpin = (Spinner) findViewById(R.id.business);
 
         if(receivedPersonInfo != null){
             nameField.setText(receivedPersonInfo.name);
-            emailField.setText(receivedPersonInfo.email);
+            busNumField.setText(receivedPersonInfo.businessNumber);
+            addressField.setText(receivedPersonInfo.address);
+            provinceSpin.setSelected(Boolean.parseBoolean(receivedPersonInfo.province));
+            businessSpin.setSelected(Boolean.parseBoolean(receivedPersonInfo.business));
         }
     }
 
     public void updateContact(View v){
-        //TODO: Update contact funcionality
+        String personID = receivedPersonInfo.uid;
+        String name = nameField.getText().toString();
+        String busNum = busNumField.getText().toString();
+        String address = addressField.getText().toString();
+        String province = provinceSpin.getSelectedItem().toString();
+        String business = businessSpin.getSelectedItem().toString();
+
+        Contact person = new Contact(personID, busNum, name, business, address, province);
+
+        appState.firebaseReference.child(personID).setValue(person);
+
+        finish();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
     }
 
     public void eraseContact(View v)
     {
-        //TODO: Erase contact functionality
+        String personID = receivedPersonInfo.uid;
+        appState.firebaseReference.child(personID).removeValue();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
+
 }
